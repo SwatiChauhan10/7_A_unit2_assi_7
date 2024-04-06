281daac004f72170786077594e4236ae86aa6392
SwatiChauhan10 <165748298+SwatiChauhan10@users.noreply.github.com>
2024-04-06 09:10:39 +0530

    Lab assignment 7

---
+++ gd.php
@@ -0,0 +1,5 @@
+# Query: <?php\nif(extension_loaded('gd')&&\nfunction_exists('gd_info'))\n{\necho "gd installed";\n}\nelse\n{\necho "gd not installed";\n}\necho phpinfo();\n?>
+# Flags: CaseSensitive
+# ContextLines: 1
+
+No Results
